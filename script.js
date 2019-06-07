0d4be34f70655f6f429d225fbf6e875c17dff252
lambord97 <dannylambord@hotmail.co.uk>
2019-06-07 13:23:17 +0100

    Adding owner functionality

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
-function fetchData(type,ext,sen) {
-    let x = sen;
 
-    return new Promise((resolve, reject)=>{
-
-            const req = new XMLHttpRequest();
-                    
-        req.onreadystatechange = () =>{
-            if (req.readyState === 4) {
-                if (req.status === 200) {
-                    resolve(req);
-                    console.log("connected");
-                } else {
-                    reject("Failed");
-                }
+let user =  {
+        id: 1,
+        firstName: "George",
+        lastName: "Franklin",
+        address: "110 W. Liberty St.",
+        city: "Madison",
+        telephone: "6085551023",
+        pets: [
+            {
+                id: 1,
+                name: "Leo",
+                birthDate: "2010/09/07",
+                type: {
+                    id: 1,
+                    name: "cat"
+                },
+                owner: 1,
+                visits: []
             }
+        ]
+    }
 
-        };
-            
-    req.open(type, "http://localhost:9092/JavaEEServer-1.0/api" + ext);
-    req.setRequestHeader("Content-Type","application/json");
-    req.send(x);
-        
-    })
+
+    function addUser(){
+        sub();
+        let s = JSON.stringify(user);
+       fetchData("POST","/owners",s).then((req)=>{ 
+    console.log(req.responseText);
+    })};
+
+function sub() {
+    user.firstName= document.getElementById("firstName").value;
+    user.lastName= document.getElementById("lastName").value;
+    user.address= document.getElementById("address").value;
+    user.city= document.getElementById("city").value;
+    user.telephone= document.getElementById("telephone").value;
+    user.pets.name= document.getElementById("petName").value;
+    user.pets.birthDate= document.getElementById("dob").value;
+
+
 }
