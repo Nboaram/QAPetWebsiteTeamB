177fbad1b08c378e9d5cc953d46b41a71f2763f7
AChar95 <allancharles95@outlook.com>
2019-06-07 11:13:31 +0100

    Completed the delete single owner script

---
+++ delete.js
@@ -0,0 +1,37 @@
+let id;
+
+const setId = (numb) => id=numb.value;
+
+
+function deleteUser() {
+    console.log(id);
+let userId = JSON.stringify(id);
+fetchData("DELETE", "/owners/" + id, userId)
+}
+
+
+function fetchData(type,ext,sen) {
+    let x = sen;
+
+    return new Promise((resolve, reject)=>{
+
+            const req = new XMLHttpRequest();
+
+        req.onreadystatechange = () =>{
+            if (req.readyState === 4) {
+                if (req.status >= 200 && req.status <300) {
+                    resolve(req);
+                    console.log("connected");
+                } else {
+                    reject("Failed");
+                }
+            }
+
+        };
+            
+    req.open(type, "http://localhost:9966/petclinic/api" + ext);
+    req.setRequestHeader("Content-Type","application/json");
+    req.send(x);
+        
+    })
+}
