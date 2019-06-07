17cdc011c8207b2c2f0451a60e773dfe47b4a213
TGilbert <Thomas.Gilbert@academytrainee.com>
2019-06-07 13:57:09 +0100

    Initial code for update owner

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function fetchData(type,ext,sen) {
+function fetchData(type, ext, sen) {
     let x = sen;
 
-    return new Promise((resolve, reject)=>{
+    return new Promise((resolve, reject) => {
 
-            const req = new XMLHttpRequest();
-                    
-        req.onreadystatechange = () =>{
+        const req = new XMLHttpRequest();
+
+        req.onreadystatechange = () => {
             if (req.readyState === 4) {
                 if (req.status === 200) {
                     resolve(req);
@@ -16,10 +16,63 @@ function fetchData(type,ext,sen) {
             }
 
         };
-            
-    req.open(type, "http://localhost:9092/JavaEEServer-1.0/api" + ext);
-    req.setRequestHeader("Content-Type","application/json");
-    req.send(x);
-        
+
+        req.open(type, "http://localhost:9092/petclinic/api/" + ext);
+        req.setRequestHeader("Content-Type", "application/json");
+        req.send(x);
+
     })
+
 }
+
+
+
+
+
+let id;
+let firstName;
+let lastName;
+let address;
+
+const setId = (num) => {id = number.value};
+
+const setFirstName = (fName) => {firstName = fName.value};
+
+const setLastName = (lName) => {lastName = lName.value};
+
+const setAddress = (addr) => {address = addr.value};
+
+
+
+function getOwner(){
+    fetchData("GET", "owners/" + id, null).then((value) => {
+    let ownerData = JSON.parse(value);
+});
+    let {owners} = ownerData;
+}
+
+function updateOwner(){
+//let owner = JSON.stringify(input);
+    fetchData('PUT', 'owners/' + id, owner);
+
+if(id === {owners}){}
+
+if (firstName !== null ){
+   myObj = {name = firstName};
+
+}
+if (lastName !== null ){
+   myObj = {name = firstName};
+
+}
+if (address !== null ){
+   myObj = {name = firstName};
+
+}
+}
+
+
+
+}
+
+
