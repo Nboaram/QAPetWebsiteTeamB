6119b4ff0356f7f09e2fbbaed758e02e919798a6
lambord97 <dannylambord@hotmail.co.uk>
2019-06-07 13:56:46 +0100

    Add owner

---
+++ addOwn.js
@@ -0,0 +1,42 @@
+
+let user =  {
+        id: 1,
+        firstName: "George",
+        lastName: "Franklin",
+        address: "110 W. Liberty St.",
+        city: "Madison",
+        telephone: "6085551023",
+        pets: [
+            {
+                id: 1,
+                name: "Leo",
+                birthDate: "2010/09/07",
+                type: {
+                    id: 1,
+                    name: "cat"
+                },
+                owner: 1,
+                visits: []
+            }
+        ]
+    }
+
+
+    function addUser(){
+        sub();
+        let s = JSON.stringify(user);
+       fetchData("POST","/owners",s).then((req)=>{ 
+    console.log(req.responseText);
+    })};
+
+function sub() {
+    user.firstName= document.getElementById("firstName").value;
+    user.lastName= document.getElementById("lastName").value;
+    user.address= document.getElementById("address").value;
+    user.city= document.getElementById("city").value;
+    user.telephone= document.getElementById("telephone").value;
+    user.pets.name= document.getElementById("petName").value;
+    user.pets.birthDate= document.getElementById("dob").value;
+
+
+}
